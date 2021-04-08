abc4095b180ff2cc120d4786b33a3f0e5750581a
Mohamed Amine Bouguerra <mabouguerra@gmail.com>
2021-04-08 10:25:13 +0100

    added (empty) schedule and about pages

---
+++ pages/about.tsx
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2020 Vercel Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Page from '@components/page';
+import About from '@components/about';
+import Header from '@components/header';
+import Layout from '@components/layout';
+
+import { META_DESCRIPTION } from '@lib/constants';
+
+export default function AboutPage() {
+  const meta = {
+    title: 'About - Annaba Techdays',
+    description: META_DESCRIPTION
+  };
+
+  return (
+    <Page meta={meta}>
+      <Layout>
+        <Header hero="About" description={meta.description} />
+        <About />
+      </Layout>
+    </Page>
+  );
+}
